491c9af723a33a12ea71d3d244202d6d1d239027
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2024-03-11 09:06:46 +0000

    string 1 and string 2

--- try_single_embedding.py
+++ try_single_embedding.py
@@ -5,13 +5,17 @@ from my_secrets import OPENAI_API_KEY
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 DIMENSIONS = 1000
+
+string_1 = "splink is a tool for data linkage"
+string_2 = "splink is software for data deduplication at scale"
+
 res1 = client.embeddings.create(
-    input=["splink is a tool for data linkage"],
+    input=[string_1],
     model="text-embedding-3-small",
     dimensions=DIMENSIONS,
 )
 res2 = client.embeddings.create(
-    input=["splink is software for data deduplication at scale "],
+    input=[string_2],
     model="text-embedding-3-small",
     dimensions=DIMENSIONS,
 )
